526bca6b22f7fcefa65c2f72ba102d8498058330
Javier Lete <jglete@gmail.com>
2019-05-09 20:47:35 +0200

    Rutado de clientes/detalles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ClientesListadoComponent } from './componentes/clientes-listado/clientes-listado.component';
+import { ClientesDetallesComponent } from './componentes/clientes-detalles/clientes-detalles.component';
 
 const routes: Routes = [
-  { path: 'clientes/listado', component: ClientesListadoComponent }
+  { path: 'clientes/listado', component: ClientesListadoComponent },
+  { path: 'clientes/detalles', component: ClientesDetallesComponent }
 ];
 
 @NgModule({
