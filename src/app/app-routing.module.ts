0540a50f36c8f2a268493b006c0d6cfd83f745ac
Javier Lete <jglete@gmail.com>
2019-05-09 20:43:22 +0200

    Rutado de listado en funcionamiento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ClientesListadoComponent } from './componentes/clientes-listado/clientes-listado.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'clientes/listado', component: ClientesListadoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
