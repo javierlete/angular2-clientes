73f80c6436eedf4bd644aa619f1b9d66569e1dbf
Javier Lete <jglete@gmail.com>
2019-05-09 20:37:25 +0200

    Tipo en la constante CLIENTES

--- src/app/mocks/mock-clientes.ts
+++ src/app/mocks/mock-clientes.ts
@@ -1,4 +1,7 @@
-export const CLIENTES = [
+import { Cliente } from '../modelo/cliente';
+
+export const CLIENTES: Cliente[] = [
     { id: 1, nombre: 'Javier', nif: '12345678Z' },
     { id: 2, nombre: 'Pepe', nif: 'Y2345678Z' },
+    { id: 3, nombre: 'Pedro', nif: '87654321A'}
 ];
