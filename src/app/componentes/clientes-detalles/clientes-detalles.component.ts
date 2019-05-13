b231b20a779e50b0d4ce342b72d3a52cea4970b8
Javier Lete <jglete@gmail.com>
2019-05-13 20:28:46 +0200

    Paso de datos por método en click

--- src/app/componentes/clientes-detalles/clientes-detalles.component.ts
+++ src/app/componentes/clientes-detalles/clientes-detalles.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from 'src/app/modelo/cliente';
 
 @Component({
   selector: 'app-clientes-detalles',
   templateUrl: './clientes-detalles.component.html',
   styleUrls: ['./clientes-detalles.component.css']
 })
-export class ClientesDetallesComponent implements OnInit {
+export class ClientesDetallesComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  guardar(id: number, nombre: string, nif: string): boolean {
+    let cliente: Cliente = { id:id, nombre:nombre, nif:nif };
+    console.log(cliente);
+    return false;
   }
-
+  
 }
