37eb286b2a0f0644693452286f298a746cf16e3b
Javier Lete <jglete@gmail.com>
2019-05-14 19:33:28 +0200

    Navegación de vuelta a listado después de añadir

--- src/app/componentes/clientes-detalles/clientes-detalles.component.ts
+++ src/app/componentes/clientes-detalles/clientes-detalles.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from 'src/app/modelo/cliente';
 import { ClientesService } from 'src/app/servicios/clientes.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-clientes-detalles',
@@ -9,14 +10,19 @@ import { ClientesService } from 'src/app/servicios/clientes.service';
 })
 export class ClientesDetallesComponent {
 
-  constructor(private clientesService: ClientesService) {}
+  constructor(
+    private clientesService: ClientesService,
+    private router: Router) {}
 
   guardar(id: number, nombre: string, nif: string): void {
     let cliente: Cliente = { id:id, nombre:nombre, nif:nif };
     console.log('EMITIDO', cliente);
 
     this.clientesService.postCliente(cliente).subscribe(
-      cliente => console.log('RECIBIDO', cliente)
+      cliente => {
+        console.log('RECIBIDO', cliente);
+        this.router.navigate(['/clientes/listado'])
+      }
     );
   }
   
