2c16c6c2589e5dbe7fb6cc95a20e387aa5690037
Javier Lete <jglete@gmail.com>
2019-05-15 19:23:05 +0200

    Generado MensajesComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { ClientesListadoComponent } from './componentes/clientes-listado/clientes-listado.component';
 import { ClientesDetallesComponent } from './componentes/clientes-detalles/clientes-detalles.component';
 import { HttpClientModule } from '@angular/common/http';
+import { MensajesComponent } from './componentes/mensajes/mensajes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ClientesListadoComponent,
-    ClientesDetallesComponent
+    ClientesDetallesComponent,
+    MensajesComponent
   ],
   imports: [
     BrowserModule,
